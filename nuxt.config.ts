94a94550cfae33e0b7d678d77b48f3657577e90f
paulo.bressan <paulo.bressan@outlook.com>
2023-12-22 10:56:48 -0300

    feat: added google analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   css: ["@/assets/css/main.css", "bootstrap-icons/font/bootstrap-icons.css"],
+  modules: ["nuxt-gtag"],
+  gtag: {
+    id: "G-5M4Z3D5QWF",
+  },
 });
